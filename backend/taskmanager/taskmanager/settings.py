5fe9905ed99e44bc0e23edb5ec8c995a87de0302
issa237 <eldiawara237@gmail.com>
2025-12-11 17:13:27 -0500

    Add /

--- backend/taskmanager/taskmanager/settings.py
+++ backend/taskmanager/taskmanager/settings.py
@@ -80,11 +80,18 @@ WSGI_APPLICATION = 'taskmanager.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='sqlite:///db.sqlite3',
+#         conn_max_age=600
+#     )
+# }
 
 
 # Password validation
